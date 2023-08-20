15856e66191236576114024d2848b68f4faeef54
khairul-01 <khairul431743@gmail.com>
2023-08-20 19:42:45 +0600

    input validation checked and area log javaScript added

--- scriptFiles/areaCalculator.js
+++ scriptFiles/areaCalculator.js
@@ -4,10 +4,20 @@ function gettingInputElement(inputId) {
    element.value = '';
    const elementValue = parseFloat(elementString);
    return elementValue;
+   // if ((typeof elementValue === 'number') && (isNaN(elementValue) === false)) {
+   // }
+   // else{
+   //    alert('Please insert valid number');
+   // }
 }
 function setElementValue(inputId, value){
    const element = document.getElementById(inputId);
-   element.innerText = value;
+   if ((typeof value === 'number') && (isNaN(value) === false)) {
+      return element.innerText = value;
+   }
+   else{
+      alert('Please insert valid number');
+   } 
 }
 // Triangle area calculator
 function triangleAreaCalculator() {
@@ -15,6 +25,13 @@ function triangleAreaCalculator() {
    const triangleHeight = gettingInputElement('triangle-height');
    const triangleArea = 0.5 * triangleBase * triangleHeight;
    setElementValue('triangle-area', triangleArea);
+   addToCalculatorLog('Triangle', triangleArea);
+   // if ((typeof triangleArea === 'number') && (isNaN(triangleArea) === false)) {
+   //    setElementValue('triangle-area', triangleArea);
+   // }
+   // else{
+   //    alert('Please insert valid number');
+   // } 
 }
 
 // rectangle area calculator
@@ -23,6 +40,7 @@ function rectangleAreaCalculator() {
    const rectangleLength = gettingInputElement('rectangle-length');
    const rectangleArea = rectangleWidth * rectangleLength;
    setElementValue('rectangle-area', rectangleArea);
+   addToCalculatorLog('rectangle', rectangleArea);
 }
 
 // parallelogram area calculator
@@ -31,6 +49,7 @@ function parallelogramAreaCalculator() {
    const parallelogramHeight = gettingInputElement('parallelogram-height');
    const parallelogramArea = parallelogramBase * parallelogramHeight;
    setElementValue('parallelogram-area', parallelogramArea);
+   addToCalculatorLog('Parallelogram', parallelogramArea);
 }
 
 // rhombus area calculator
@@ -39,6 +58,7 @@ function rhombusAreaCalculator() {
    const rhombusD2 = gettingInputElement('rhombus-d2');
    const rhombusArea = 0.5 * rhombusD1 * rhombusD2;
    setElementValue('rhombus-area', rhombusArea);
+   addToCalculatorLog('Rhombus', rhombusArea);
 }
 
 // pentagon area calculator
@@ -47,12 +67,32 @@ function pentagonAreaCalculator() {
    const pentagonB = gettingInputElement('pentagon-b');
    const pentagonArea = 0.5 * pentagonP * pentagonB;
    setElementValue('pentagon-area', pentagonArea);
+   addToCalculatorLog('Pentagon', pentagonArea);
 }
 
 // ellipse area calculator
 function ellipseAreaCalculator() {
    const ellipseA = gettingInputElement('ellipse-a');
    const ellipseB = gettingInputElement('ellipse-b');
-   const ellipseArea = (Math.PI * ellipseA * ellipseB).toFixed(4);
+   const ellipseArea = parseFloat((Math.PI * ellipseA * ellipseB).toFixed(3));
    setElementValue('ellipse-area', ellipseArea);
+   addToCalculatorLog('Ellipse', ellipseArea);
+}
+
+// add results log to area calculator
+/**
+ * create an element you want to add
+ * if needed add some class
+ * set inner html. it could be dynamic Template string
+ * append the created element as child of parent element
+ */
+function addToCalculatorLog(areaType, area) {
+   console.log(areaType+' '+area);
+   const areaLog = document.getElementById('area-log');
+   const count = areaLog.childElementCount + 1;
+   const element = document.createElement('p')
+   element.classList.add('my-3')
+   element.innerHTML = 
+   `${count} ${areaType} ${area} cm<sup>2</sup>  <button class="btn btn-success btn-sm">Convert</button>`;
+   areaLog.appendChild(element);
 }
